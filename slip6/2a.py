896af469e8be4b0d3641312953e51990c890a3f9
Dvs007 <118052898+Dvs007@users.noreply.github.com>
2023-10-18 22:50:29 +0530

    Add files via upload

---
+++ slip6/2a.py
@@ -0,0 +1,14 @@
+import math
+
+l = float(input("Enter length of cube:"))
+
+print("SA of a cube is:" ,6*pow(l,2))
+
+print("Volume of a cube is:" ,pow(l,3))
+
+r = float(input("Enter radius of sphere:"))
+
+print("SA of a sphere is:" ,4*math.pi*pow(r,2))
+
+print("Volume of a sphere is:" ,(4/3)*math.pi*pow(r,3))
+
